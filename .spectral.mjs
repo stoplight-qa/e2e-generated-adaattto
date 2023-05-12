c0f87dc38f82557d5cd5dcfe14835d18f0112c80
qa-qajhm-1-deou@example.com <qa-qajhm-1-deou@example.com>
2023-05-12 21:37:37 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-16461.stoplight-dev.com/api/v1/projects/cHJqOjExMjQ/spectral.js?branch_slug=main&token=00aaf624-a464-4a2d-9a8c-82e3c97a02f6";
+export default { extends: ruleset };
